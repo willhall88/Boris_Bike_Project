3469266c8f7896ec1f7dadd8b3e6c3befe4621b1
Will Hall <willhall88@hotmail.com>
2014-04-14 16:22:50 +0100

    added a new test for capacity of the station.

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,8 @@
 class DockingStation
-  def initialize
+  DEFAULT_CAPACITY = 10
+
+  def initialize(options = {})
+    @capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
     @bikes = []
   end
 
@@ -13,6 +16,10 @@ class DockingStation
 
   def release(bike)
     @bikes.delete(bike)
+  end
+
+  def full?
+    bike_count == @capacity
   end
 
 end
