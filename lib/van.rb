c2c7f6809df1d50532e81f7b1a3ca7b5b00a652e
Will Hall <willhall88@hotmail.com>
2014-04-16 12:41:24 +0100

    cleaned up van.rb

--- lib/van.rb
+++ lib/van.rb
@@ -6,21 +6,11 @@ class Van
   def initialize(options = {})
     self.capacity = options.fetch(:capacity, capacity)
   end
-    
-  # def collect_bikes_from(station)
-  #   self.dock(station.release_broken_bike)
-  # end
-  
+      
   def collect_bikes_from(station)
     return if self.full?
       self.dock(station.release_broken_bike)
       collect_bikes_from(station)
-    # end
   end
-  #   station.release_broken_bikes
-  #   station.broken_bikes.each {|bike| bikes << bike}
-  # end
-
-
 
 end
